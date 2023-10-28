ffcac069d9b462e0b47bfb6b6a5f2999d61e2935
achoolucgust <lucgustga@outlook.com>
2023-10-28 16:13:37 +0800

    Update checker actually works

--- updatechecker.py
+++ updatechecker.py
@@ -1,4 +1,6 @@
 import requests
 with open("version","r") as f:
-    if requests.get("https://raw.githubusercontent.com/gustwho/gusts-prompt-generator/main/version").text != f.read():
-        input("Theres a new version! Download it at https://github.com/gustwho/gusts-prompt-generator/\n\n[OK]")
+    latest = int(requests.get("https://raw.githubusercontent.com/gustwho/gusts-prompt-generator/main/version").text)
+    current = int(f.read())
+    if latest > current:
+        input(f"Theres a new version! Download it at https://github.com/gustwho/gusts-prompt-generator/\n\nYou are {latest-current} versions behind!\n\n[OK]")
