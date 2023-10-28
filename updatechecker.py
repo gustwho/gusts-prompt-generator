e374927ea7db5ebd828b6db0078a2a7d873b9c52
achoolucgust <lucgustga@outlook.com>
2023-10-28 16:11:36 +0800

    jesus christ man

--- updatechecker.py
+++ updatechecker.py
@@ -1,4 +1,4 @@
 import requests
 with open("version","r") as f:
-    if requests.get("https://raw.githubusercontent.com/gustwho/gusts-prompt-generator/main/version") != f.read():
+    if requests.get("https://raw.githubusercontent.com/gustwho/gusts-prompt-generator/main/version").text != f.read():
         input("Theres a new version! Download it at https://github.com/gustwho/gusts-prompt-generator/\n\n[OK]")
